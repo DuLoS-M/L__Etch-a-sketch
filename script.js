f481566e84d6a6b23e7017d9d0c38d83655b0bd5
DuLoS-M <e_craud@hotmail.com>
2022-08-04 11:14:53 -0600

    Added logic to change the amount of pixels with slider, adjusted logic from createGrid

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 const gridContainer = document.querySelector('#grid-container');
+const pixelCountSlider = document.querySelector('#grid-size-slider')
 
-let numberOfPixels = 100
-let squareDimentions = 900 / numberOfPixels
 
 // Create grid
-function createGrid() {
 
-    for (let i = 0; i < numberOfPixels**2; i++) {
+function createGrid(numOfRows) {
+
+    let pixelDimms = 900 / numOfRows
+
+    for (let i = 0; i < numOfRows**2; i++) {
         const gridPixel = document.createElement('div');
 
         
         gridPixel.classList.toggle('pixel');
         gridPixel.textContent = ' ';
-        gridPixel.style.width = `${squareDimentions}px`;
-        gridPixel.style.height = `${squareDimentions}px`;
+        gridPixel.style.width = `${pixelDimms}px`;
+        gridPixel.style.height = `${pixelDimms}px`;
         gridPixel.setAttribute('draggable', false)
         gridContainer.appendChild(gridPixel);
         
     }
 }
+
+// Set default 10x10 grid
+createGrid(10)
+
+// Adjust number of pixels with the slider
+pixelCountSlider.addEventListener('click', () => {
+
+    const currentPixels = document.querySelectorAll('.pixel')
+    // If the container already has pixels, remove them
+    if (currentPixels.length !== 0) {
+        currentPixels.forEach(element => {
+            gridContainer.removeChild(element)
+        });
+    }
+
+    let numberOfRows = pixelCountSlider.value
+    createGrid(numberOfRows)
+
+})
+
 
 
 const grid = document.querySelector("#grid-container");
